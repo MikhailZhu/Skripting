c124a4f97d3bfb752c736050f2af2ea34c6ec88f
MikhailZhu <119092036+MikhailZhu@users.noreply.github.com>
2022-11-26 09:55:03 +0600

    Add files via upload

---
+++ Heloworld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(" Heloworld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
